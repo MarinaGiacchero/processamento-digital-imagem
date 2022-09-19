7c88d93e9d7f725001b3e50659f4f7c508141fad
Marina Giacchero <marinagiacchero@hotmail.com>
2022-09-19 14:40:03 -0300

    Organização dos arquivos
    Foi feita uma organização dos arquivos para que facilitar o acesso e visualização.

---
+++ transformacoes_histograma/transf_Log.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+img = cv.imread('img/Fig308.tif')
+#img = cv.imread('img/enhance-me.gif') #segunda imagem
+c = 255 / np.log(1 + np.max(img))
+log_img = c * (np.log(img + 1))
+log_img = np.array(log_img, dtype = np.uint8)  
+#apresentar na tela
+fig, ax = plt.subplots(nrows=2,ncols=2)
+ax[0,0].imshow(img,cmap='gray')
+ax[0,0].set_title('Imagem original')  
+ax[0,1].imshow(log_img,cmap='gray')
+ax[0,1].set_title('Imagem transformada')
+ 
+plt.show()
